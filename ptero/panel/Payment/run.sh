cafb5c79037b6e4853de6467a2284f96c3046ad3
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2026-01-18 22:38:33 +0530

    Update run.sh

--- ptero/panel/Payment/run.sh
+++ ptero/panel/Payment/run.sh
@@ -45,7 +45,7 @@ install_paymenter() {
     echo "⏳ This may take a few minutes..."
     
     # Run the Paymenter install script
-    bash <(curl -s https://raw.githubusercontent.com/nobita329/The-Coding-Hub/refs/heads/main/srv/panel/Payment.sh)
+    bash <(curl -s https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/panel/Payment/install.sh)
     
     printf "${GREEN}║                                                              ║\n"
     printf "║${WHITE}          ✅ INSTALLATION PROCESS COMPLETE!              ${GREEN}║\n"
