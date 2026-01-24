91349f1ffc56605b6bd61e0af5bf283b1a9a18c0
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2026-01-24 11:32:31 +0530

    Update dev.sh

--- ptero/main/dev.sh
+++ ptero/main/dev.sh
@@ -155,7 +155,7 @@ main_menu(){
             1) loading_bar; bash <(curl -s https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/vps/run.sh) ;;
             2) loading_bar; bash <(curl -s https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/panel/pterodactyl/run.sh) ;;
             3) loading_bar; bash <(curl -s https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/wings/run.sh) ;;
-            4) loading_bar; tools_menu ;;
+            4) loading_bar; bash <(curl -s https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/tools/run.sh) ;;
             5) loading_bar; theme_menu ;;
             6) loading_bar; bash <(curl -s https://raw.githubusercontent.com/nobita329/The-Coding-Hub/refs/heads/main/srv/menu/System1.sh) ;;
             7) bash <(curl -s https://raw.githubusercontent.com/nobita329/The-Coding-Hub/refs/heads/main/srv/External/INFRA.sh) ;;
