4a5c0a2e29ad8117832a90df36d8f678153efd9b
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2026-02-04 22:57:09 +0530

    Update run.sh

--- ptero/panel/pterodactyl/run.sh
+++ ptero/panel/pterodactyl/run.sh
@@ -135,7 +135,6 @@ update_panel() {
     status_msg "INFO" "Putting panel into Maintenance Mode..."
     cd /var/www/pterodactyl
     php artisan down
-    rm -rf 
     cd /var/www/pterodactyl
     status_msg "INFO" "Downloading latest release..."
     curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
